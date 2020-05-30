f24022a1ead1e555a6025bf05becfb252d6783a0
Layer8Err <dwight.brenner@gmail.com>
2020-05-29 17:02:46 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # WSL2setup
+
+Quickly get up and running with WSL2 on Windows 10 Build 2004.
+
+## Quickstart
+
+To quickly get up and running with WSL2, you can run the following one-liner:
+
+```posh
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Layer8Err/WSL2setup/master/WSL2_Install.ps1'))
+```
---
+++ WSL2_Install.ps1
@@ -0,0 +1,34 @@
+# Install WSL
+# This script needs to be run as a priviledged user
+#dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+$wslinst = Enable-WindowsOptionalFeature -Online  -NoRestart -FeatureName Microsoft-Windows-Subsystem-Linux
+
+# Enable Virtual Machine Platform
+#dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+$vmpinst = Enable-WindowsOptionalFeature -Online  -NoRestart -FeatureName VirtualMachinePlatform
+
+# At this point a reboot is probably necessary
+
+function Test-RebootRequired {
+    if (($wslinst.RestartNeeded -eq $true) -or ($vmpinst.RestartNeeded -eq $true)) {
+        return $true
+    } else {
+        return $false
+    }
+}
+
+# Install Ubuntu 18.04 LTS
+function Install-Ubuntu () {
+    $URL = 'https://aka.ms/wsl-ubuntu-1804'
+    $Filename = "$(Split-Path $URL -Leaf).appx"
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest -Uri $URL -OutFile $Filename -UseBasicParsing
+    Invoke-Item $FileName
+}
+
+if (Test-RebootRequired){
+    shutdown /t 120 /r /c "Reboot required to finish installing WSL2"
+} else {
+    Install-Ubuntu
+    wsl --set-default-version 2 # Set WSL2 as the default
+}
