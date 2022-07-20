db44ea621a90d7a7ac016bb94c745bb9f695d25c
Layer8Err <dwight.brenner@gmail.com>
2022-07-20 11:14:23 -0400

    Add option to install Ubuntu 22.04

--- WSL2_Install.ps1
+++ WSL2_Install.ps1
@@ -157,6 +157,13 @@ function Select-Distro () {
     # You can also use https://store.rg-adguard.net to get Appx links from Windows Store links
     $distrolist = (
         [PSCustomObject]@{
+            'Name' = 'Ubuntu 22.04'
+            'URI' = 'https://aka.ms/wslubuntu2204'
+            'AppxName' = 'CanonicalGroupLimited.Ubuntu22.04onWindows'
+            'winpe' = 'ubuntu2204.exe'
+            'installed' = $false
+        },
+        [PSCustomObject]@{
             'Name' = 'Ubuntu 20.04'
             'URI' = 'https://aka.ms/wslubuntu2004'
             'AppxName' = 'CanonicalGroupLimited.Ubuntu20.04onWindows'
